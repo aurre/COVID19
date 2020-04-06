5289d6c1aa24790fca763c3047802ee690873459
raysa <aurrecochearaysa@gmail.com>
2020-04-05 19:11:47 -0500

    edit vue config

--- vue.config.js
+++ vue.config.js
@@ -32,10 +32,10 @@ const vueConfig = {
      devServer: {
     // development server port 8000
     port: 8000,
-    // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
+    // If you want to turn on the proxy
     //   proxy: {
     //   '/api': {
-    //     target: 'http://localhost:5000',
+    //     target: '',
     //     ws: true,
     //     changeOrigin: true
     //   },
